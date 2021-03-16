2b21586cbd9ecde1bac7774381b467faad34bb3a
BerlinRob <berlin.rob@lifeinthedot.com>
2021-03-15 22:41:42 -0500

    added basic JoyStick header and class file

---
+++ lib/JoyStick/src/cflib_JoyStick.h
@@ -0,0 +1,18 @@
+#ifndef cflib_JoyStick
+#define cflib_JoyStick
+
+class cfjoystick{
+    public:
+        // constructor
+        cfjoystick(bool dosomething = false);
+
+        // methods
+        void begin();
+        void loop();
+
+    private:
+
+
+};
+
+#endif
---
+++ lib/JoyStick/src/cfsJoyStick.cpp
@@ -0,0 +1,16 @@
+#include "cflib_JoyStick.h"
+
+cfjoystick::cfjoystick(bool DoSomething){
+    // things to do on instantination
+
+}
+
+void cfjoystick::begin(){
+
+
+}
+
+void cfjoystick::loop(){
+
+
+}
